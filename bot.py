988f611c8c34e17e79cf5c223e82c68b763f65fa
Jovan Medford <jovan_medford@hotmail.com>
2018-12-06 22:25:04 -0500

    Added twitter bot python program

---
+++ bot.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import time
+import tweepy
+from tweepy import OAuthHandler
+from os import environ
+
+#ck = '78XhHsIjIsFUn2qhUI0rwSKcA' #(API key)
+#csk = 'cH3WvmIUXGdqVtwrMho0sbPQQGTLlpUB4XJ1LusVJetb7dNmKZ' #(API secret key)
+#oat = '1068347698284236800-XkscYACRZXnNuBJQAQMZ40Ck4CykWr' #(Access token)
+#oats = 'pXADhvlfoWAc95STBHSEIksMl4QD12EmmdzVN09eB1A6e' #(Access token secret)
+
+ck = environ['CONSUMER_KEY']
+csk = environ['CONSUMER_SECRET']
+oat = environ['ACCESS_KEY']
+oats = environ['ACCESS_SECRET']
+
+auth = OAuthHandler(ck, csk)
+auth.set_access_token(oat,oats)
+api = tweepy.API(auth)
+
+api.update_status("")
+time.sleep(5)
