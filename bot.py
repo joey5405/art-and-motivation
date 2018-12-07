cba1c52c06dc89145c14cea62331b8fef020d481
Jovan Medford <jovan_medford@hotmail.com>
2018-12-06 23:53:54 -0500

    changed oauth variables to suit server environment variables

--- bot.py
+++ bot.py
@@ -4,15 +4,11 @@ from tweepy import OAuthHandler
 from os import environ
 
 
-#ck = '78XhHsIjIsFUn2qhUI0rwSKcA' #(API key)
-#csk = 'cH3WvmIUXGdqVtwrMho0sbPQQGTLlpUB4XJ1LusVJetb7dNmKZ' #(API secret key)
-#oat = '1068347698284236800-XkscYACRZXnNuBJQAQMZ40Ck4CykWr' #(Access token)
-#oats = 'pXADhvlfoWAc95STBHSEIksMl4QD12EmmdzVN09eB1A6e' #(Access token secret)
-
-ck = ''
-csk = ''
-oat = ''
-oats = ''
+from os import environ
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 auth = OAuthHandler(ck, csk)
 auth.set_access_token(oat,oats)
