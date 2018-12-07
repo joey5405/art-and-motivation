c489bb5f50e3a4d1f2b0a0d0c22b073fa958049f
Jovan Medford <jovan_medford@hotmail.com>
2018-12-06 22:48:50 -0500

    Changed "woker" to "web" in procfile Changed OAUth to empty quotes for each

--- bot.py
+++ bot.py
@@ -1,19 +1,21 @@
-#!/usr/bin/python
-
 import time
 import tweepy
 from tweepy import OAuthHandler
 from os import environ
+from flask import Flask
+
+app = Flask(__name__)
+app.run(host = '0.0.0.0', port=environ.get('PORT'))
 
 #ck = '78XhHsIjIsFUn2qhUI0rwSKcA' #(API key)
 #csk = 'cH3WvmIUXGdqVtwrMho0sbPQQGTLlpUB4XJ1LusVJetb7dNmKZ' #(API secret key)
 #oat = '1068347698284236800-XkscYACRZXnNuBJQAQMZ40Ck4CykWr' #(Access token)
 #oats = 'pXADhvlfoWAc95STBHSEIksMl4QD12EmmdzVN09eB1A6e' #(Access token secret)
 
-ck = environ['CONSUMER_KEY']
-csk = environ['CONSUMER_SECRET']
-oat = environ['ACCESS_KEY']
-oats = environ['ACCESS_SECRET']
+ck = ''
+csk = ''
+oat = ''
+oats = ''
 
 auth = OAuthHandler(ck, csk)
 auth.set_access_token(oat,oats)
